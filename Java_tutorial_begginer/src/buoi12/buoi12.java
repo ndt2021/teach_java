5beecba2e977a7590742b32c966fc3bc1a924bfa
tú <nguyenductu2002tu@gmail.com>
2024-01-22 21:59:01 +0700

    1

--- Java_tutorial_begginer/src/buoi12/buoi12.java
+++ Java_tutorial_begginer/src/buoi12/buoi12.java
@@ -9,12 +9,13 @@ public class buoi12 {
         int[] arr = new int[n];
 
         for(int i=0; i <= n-1; i++){
-            System.out.println("arr["+i+"]= ");
+           // System.out.println("arr["+i+"]= ");
             arr[i] = sc.nextInt();
         }
 
         for (int i =0; i< arr.length;i++ ){
-            System.out.println("arr["+i+"]=" + arr[i]);
+            //System.out.println("arr["+i+"]=" + arr[i]);
+            System.out.println(arr[i]);
         }
 
     }

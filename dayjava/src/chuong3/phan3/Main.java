a5d16d9eecdbef8e8cf3b0b079eae3ca2a59141f
tú <nguyenductu2002tu@gmail.com>
2024-02-27 21:58:43 +0700

    1

--- dayjava/src/chuong3/phan3/Main.java
+++ dayjava/src/chuong3/phan3/Main.java
@@ -2,18 +2,19 @@ package chuong3.phan3;
 
 public class Main {
     public static void main(String[] args) {
-       /* int x = 3 ;
+        /*int x = 3 ;
         int y =4;
         int a = ( x>y ) ? 1 : 2;
         System.out.println(" a = " + a);*/
 
         int a = 201;
-        var kq ="";
-        if(a%2==0){
-            kq = "so chan";
-        }else kq = "so le";
+        var kq = "";
 
-        kq = (a%2==0) ? "so chan" : "so le";
+//        if(a%2==0){
+//            kq = "so chan";
+//        }else kq = "so le";
+
+       kq = (a%2==0) ? "so chan" : "so le";
 
         System.out.println(kq);
     }
